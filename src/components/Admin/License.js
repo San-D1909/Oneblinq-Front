08e2d9787054e38008e8307727d4bbe4a7cd370e
onurinc <onur.i@hotmail.nl>
2021-12-20 11:06:02 +0100

    Added Company Info in License Admin Dashboard

--- src/components/Admin/License.js
+++ src/components/Admin/License.js
@@ -35,6 +35,7 @@ export const LicenseList = (props) => (
   <List {...props}>
     <Datagrid>
       <TextField source="licenseKey" label="License key" />
+      <TextField source="licenseType.typeName" label="License Type" />
       <TextField source="licenseType.maxAmount" label="Limit" />
       <TextField source="timesActivated" label="Amount activated" />
       <BooleanField source="isActive" label="Is Active" />
@@ -55,8 +56,15 @@ export const LicenseShow = (props) => (
     <SimpleShowLayout>
       <TextField source="licenseKey" label="License key" />
       <TextField source="licenseType.maxAmount" label="Limit" />
+      <TextField source="licenseType.typeName" label="License Type" />
       <TextField source="timesActivated" label="Amount activated" />
       <BooleanField source="isActive" label="Is Active" />
+      <DateField source="company.companyName" label="Company Name" />
+      <DateField source="company.zipCode" label="Zip Code" />
+      <DateField source="company.street" label="Street" />
+      <DateField source="company.houseNumber" label="House number" />
+      <DateField source="company.btwNumber" label="BTW Number" />
+      <DateField source="company.phoneNumber" label="Phone Number" />
       <DateField source="creationTime" label="Created on" />
       <DateField source="expirationTime" label="Expires on" />
     </SimpleShowLayout>
