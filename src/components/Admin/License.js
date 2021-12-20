2ad59e8a5d4bfcc41db25985afdbfc96e695168a
onurinc <onur.i@hotmail.nl>
2021-12-20 10:41:15 +0100

    IsActive Edit Function generated in License Admin Dashboard.

--- src/components/Admin/License.js
+++ src/components/Admin/License.js
@@ -1,40 +1,77 @@
 // in src/posts.js
 import * as React from "react";
-import { List, Datagrid, TextField, DateField, BooleanField, ImageInput, TranslatableInputs, ImageField, NumberInput, TranslatableFields, Show, SimpleShowLayout, RichTextField, NumberField, ShowButton } from 'react-admin';
-import { Create, Edit, SimpleForm, TextInput, DateInput, ReferenceManyField, EditButton, required } from 'react-admin';
-import RichTextInput from 'ra-input-rich-text';
+import {
+  List,
+  Datagrid,
+  TextField,
+  DateField,
+  BooleanField,
+  ImageInput,
+  TranslatableInputs,
+  ImageField,
+  NumberInput,
+  TranslatableFields,
+  Show,
+  SimpleShowLayout,
+  RichTextField,
+  NumberField,
+  ShowButton,
+  PasswordInput,
+  BooleanInput,
+} from "react-admin";
+import {
+  Create,
+  Edit,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  ReferenceManyField,
+  EditButton,
+  required,
+} from "react-admin";
+import RichTextInput from "ra-input-rich-text";
 
 export const LicenseList = (props) => (
-    <List {...props}>
-        <Datagrid>
-            <TextField source="licenseKey" label="License key"/>
-            <TextField source="plugin.pluginName" label="Plugin name" />
-            <TextField source="licenseType.maxAmount" label="Limit" />
-            <TextField source="timesActivated" label="Amount activated" />
-            <DateField source="creationTime" label="Created on" />
-            <DateField source="expirationTime" label="Expires on" />
-            <LicenseShowButton {...props} />
-        </Datagrid>
-    </List>
+  <List {...props}>
+    <Datagrid>
+      <TextField source="licenseKey" label="License key" />
+      <TextField source="licenseType.maxAmount" label="Limit" />
+      <TextField source="timesActivated" label="Amount activated" />
+      <BooleanField source="isActive" label="Is Active" />
+      <DateField source="creationTime" label="Created on" />
+      <DateField source="expirationTime" label="Expires on" />
+      <LicenseShowButton {...props} />
+    </Datagrid>
+  </List>
 );
 
 const LicenseShowButton = ({ record }) => {
-    console.log(record);
-    return (
-    <ShowButton basePath="license" label="Show info" record={ record } />
-        );
-    
-}
+  console.log(record);
+  return <ShowButton basePath="license" label="Show info" record={record} />;
+};
 
 export const LicenseShow = (props) => (
-    <Show {...props}>
-        <SimpleShowLayout>
-            <TextField source="licenseKey" label="License key" />
-            <TextField source="plugin.pluginName" label="Plugin name" />
-            <TextField source="licenseType.maxAmount" label="Limit" />
-            <TextField source="timesActivated" label="Amount activated" />
-            <DateField source="creationTime" label="Created on" />
-            <DateField source="expirationTime" label="Expires on" />
-        </SimpleShowLayout>
-    </Show>
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="licenseKey" label="License key" />
+      <TextField source="licenseType.maxAmount" label="Limit" />
+      <TextField source="timesActivated" label="Amount activated" />
+      <BooleanField source="isActive" label="Is Active" />
+      <DateField source="creationTime" label="Created on" />
+      <DateField source="expirationTime" label="Expires on" />
+    </SimpleShowLayout>
+  </Show>
+);
+
+export const LicenseEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput
+        source="timesActivated"
+        label="Amount activated"
+        validate={required()}
+      />
+      <BooleanInput source="isActive" label="Is Active" validate={required()} />
+    </SimpleForm>
+  </Edit>
 );
